cc81a1c9555010b89177ea615ca0c20a77f3f62b
garrett <gfitzgerald123@gmail.com>
2024-05-18 11:15:02 -0500

    entering otp automatically submits

--- src/components/Modal/IonModal.jsx
+++ src/components/Modal/IonModal.jsx
@@ -8,36 +8,36 @@ import VeriffLogo from "../../assets/images/veriff-logo.svg";
 import VeriffLogoBtn from "../../assets/images/veriff-logo-button.svg";
 
 const IonModalComponent = ({ isOpen, onClose }) => {
-  const history = useHistory();
+	const history = useHistory();
 
-  const onConfirm = () => {
-    history.push("/account-setup");
-  };
+	const onConfirm = () => {
+		history.push("/account-setup");
+	};
 
-  return (
-    <IonModal isOpen={isOpen} onDidDismiss={onClose} cssClass="veriff-modal">
-      <IonContent
-        style={{
-          "--background": "#303136",
-          "--color": "white",
-        }}
-      >
-        <IonIcon icon={close} onClick={onClose} className="modal-close-icon" />
-        <img src={VeriffLogo} alt="veriff-logo" className="content-img"></img>
-        <p className="content-text">
-          The Matchmaker AI has partnered with Veriff to verify your income
-        </p>
-        <button onClick={onConfirm} className="veriff-button">
-          <img
-            src={VeriffLogoBtn}
-            alt="veriff-logo-button"
-            className="veriff-logo-button"
-          ></img>
-          Continue with Veriff
-        </button>
-      </IonContent>
-    </IonModal>
-  );
+	return (
+		<IonModal isOpen={isOpen} onDidDismiss={onClose} cssClass="veriff-modal">
+			<IonContent
+				style={{
+					"--background": "#303136",
+					"--color": "white",
+				}}
+			>
+				<IonIcon icon={close} onClick={onClose} className="modal-close-icon" />
+				<img src={VeriffLogo} alt="veriff-logo" className="content-img"></img>
+				<p className="content-text">
+					The Matchmaker AI has partnered with Veriff to verify your identity.
+				</p>
+				<button onClick={onConfirm} className="veriff-button">
+					<img
+						src={VeriffLogoBtn}
+						alt="veriff-logo-button"
+						className="veriff-logo-button"
+					></img>
+					Continue with Veriff
+				</button>
+			</IonContent>
+		</IonModal>
+	);
 };
 
 export default IonModalComponent;
--- src/pages/GetCode/index.tsx
+++ src/pages/GetCode/index.tsx
@@ -2,12 +2,12 @@ import "./GetCode.scss";
 import React, { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
 import {
-  IonPage,
-  IonInput,
-  IonTitle,
-  IonButton,
-  IonContent,
-  IonIcon,
+	IonPage,
+	IonInput,
+	IonTitle,
+	IonButton,
+	IonContent,
+	IonIcon,
 } from "@ionic/react";
 import { closeOutline } from "ionicons/icons";
 import axios from "axios";
@@ -21,200 +21,199 @@ import { getAuth, RecaptchaVerifier } from "firebase/auth";
 import IonModalComponent from "../../components/Modal/IonModal";
 
 const GetCode: React.FC = (props) => {
-  const params = useHistory();
-  // const [otp, setOtp] = useState("");
-  const [otpInputs, setOtpInputs] = useState(["", "", "", "", "", ""]);
-  const ionInputEls = useRef<Array<HTMLIonInputElement>>([]);
-  const [isModalOpen, setIsModalOpen] = useState(false);
+	const params = useHistory();
+	// const [otp, setOtp] = useState("");
+	const [otpInputs, setOtpInputs] = useState(["", "", "", "", "", ""]);
+	const ionInputEls = useRef<Array<HTMLIonInputElement>>([]);
+	const [isModalOpen, setIsModalOpen] = useState(false);
 
-  // const ionInputEl = useRef<HTMLIonInputElement>(null);
-  // const [confirmationResult, setConfirmationResult] = useState(null);
-  const confirmationResult = useSelector(
-    (state: any) => state.HomeReducer.confirmResult
-  );
+	// const ionInputEl = useRef<HTMLIonInputElement>(null);
+	// const [confirmationResult, setConfirmationResult] = useState(null);
+	const confirmationResult = useSelector(
+		(state: any) => state.HomeReducer.confirmResult
+	);
 
-  const recaptchaRef = React.createRef();
+	const recaptchaRef = React.createRef();
 
-  const generatorRecaptcha = () => {
-    (window as any).recaptchaVerifier = new RecaptchaVerifier(
-      "recaptcha-container",
-      {
-        size: "invisible",
-        callback: async (response) => {
-          // Reset the reCAPTCHA after 500ms
-          console.log("response", response);
-          setTimeout(() => {
-            console.log("recaptchaRef", recaptchaRef);
+	const generatorRecaptcha = () => {
+		(window as any).recaptchaVerifier = new RecaptchaVerifier(
+			"recaptcha-container",
+			{
+				size: "invisible",
+				callback: async (response) => {
+					// Reset the reCAPTCHA after 500ms
+					console.log("response", response);
+					setTimeout(() => {
+						console.log("recaptchaRef", recaptchaRef);
 
-            const ref = recaptchaRef.current as { reset: () => void }; // Asserting the type
-            if (ref) {
-              ref.reset();
-            }
-          }, 500);
-        },
-      },
-      getAuth()
-    );
-  };
+						const ref = recaptchaRef.current as { reset: () => void }; // Asserting the type
+						if (ref) {
+							ref.reset();
+						}
+					}, 500);
+				},
+			},
+			getAuth()
+		);
+	};
 
-  // useEffect(() => {
-  //   if (
-  //     params?.location?.search !== null &&
-  //     params?.location?.search !== undefined
-  //   ) {
-  //     const jsonString = params?.location?.pathname;
-  //     const jsonPart = jsonString.split(":").slice(1).join(":").trim();
-  //     // Removing escape characters
-  //     const cleanedJsonString = jsonPart.replace(/\\/g, "");
-  //     // Parsing JSON
-  //     const jsonData = JSON.parse(cleanedJsonString);
-  //     // Extracting jsonData
-  //     setConfirmationResult(jsonData);
-  //   }
-  // }, []);
+	// useEffect(() => {
+	//   if (
+	//     params?.location?.search !== null &&
+	//     params?.location?.search !== undefined
+	//   ) {
+	//     const jsonString = params?.location?.pathname;
+	//     const jsonPart = jsonString.split(":").slice(1).join(":").trim();
+	//     // Removing escape characters
+	//     const cleanedJsonString = jsonPart.replace(/\\/g, "");
+	//     // Parsing JSON
+	//     const jsonData = JSON.parse(cleanedJsonString);
+	//     // Extracting jsonData
+	//     setConfirmationResult(jsonData);
+	//   }
+	// }, []);
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    if (otpInputs.length === 6 && confirmationResult) {
-      try {
-        let confirmResult = confirmationResult;
-        console.log({ confirmResult });
-        confirmResult
-          .confirm(otpInputs.join(""))
-          .then(async (result) => {
-            console.log("Phone Verified Successfully - user", result.user);
-            toast("Phone Verified Successfully");
-            const user = result.user;
-            const userDoc = doc(db, "Users", user.uid);
-            const userDocData = await getDoc(userDoc);
-            console.log("userDocData", userDocData.exists());
-            setIsModalOpen(true);
+	useEffect(() => {
+		// Check if all OTP inputs are filled and length is 6
+		if (otpInputs.length === 6 && otpInputs.every((input) => input !== "")) {
+			generatorRecaptcha(); // Ensure reCAPTCHA is generated
+			handleSubmit(new Event("submit")); // Programmatically trigger handleSubmit
+		}
+	}, [otpInputs]); // Dependency array includes otpInputs to trigger effect when it changes
 
-            if (userDocData.exists()) {
-              // If user exists, navigate to /chat_page
-              console.log("Navigating to /chat_page");
-              // localStorage.setItem("user", JSON.stringify(user));
+	const handleSubmit = async (event) => {
+		event.preventDefault();
+		console.log("handleSubmit", otpInputs);
+		console.log("joined inputs:", otpInputs.join(""));
+		if (otpInputs.length === 6 && confirmationResult) {
+			try {
+				let confirmResult = confirmationResult;
+				console.log("confirmResult", confirmResult);
+				confirmResult
+					.confirm(otpInputs.join(""))
+					.then(async (result) => {
+						console.log("Phone Verified Successfully - user", result.user);
+						toast("Phone Verified Successfully");
+						const user = result.user;
+						const userDoc = doc(db, "Users", user.uid);
+						const userDocData = await getDoc(userDoc);
+						console.log("userDocData", userDocData.exists());
+						setIsModalOpen(true);
 
-              // const response = await axios.get(
-              //   `${SERVER_BASE_URL}/users/${user.uid}`
-              // );
-              // const userDetails = response.data;
-            } else {
-            }
-          })
-          .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            console.log("otp_Error", errorCode, errorMessage);
-            if (errorCode === "auth/invalid-verification-code") {
-              toast("Invalid verification Code");
-            } else {
-              toast("Phone Verification Failed");
-            }
-          });
-      } catch (e) {
-        console.log("verifyOTP_Error", e);
-      }
-    } else {
-      toast("Please enter your number we texted you");
-    }
-  };
+						if (userDocData.exists()) {
+							// If user exists, navigate to /chat_page
+							console.log("Navigating to /chat_page");
+							// localStorage.setItem("user", JSON.stringify(user));
 
-  const onVerifyCode = (ev: Event) => {
-    const target = ev.target as HTMLIonInputElement;
-    const inputIndex = parseInt(target.getAttribute("data-index") || "0", 10);
-    const value = (ev.target as HTMLIonInputElement).value as string;
+							// const response = await axios.get(
+							//   `${SERVER_BASE_URL}/users/${user.uid}`
+							// );
+							// const userDetails = response.data;
+						} else {
+						}
+					})
+					.catch((error) => {
+						const errorCode = error.code;
+						const errorMessage = error.message;
+						console.log("otp_Error", errorCode, errorMessage);
+						if (errorCode === "auth/invalid-verification-code") {
+							toast("Invalid verification Code");
+						} else {
+							toast("Phone Verification Failed");
+						}
+					});
+			} catch (e) {
+				console.log("verifyOTP_Error", e);
+			}
+		} else {
+			toast("Please enter your number we texted you");
+		}
+	};
 
-    // Ensure the value is a single digit
-    const filteredValue = value.replace(/\D/g, "").substring(0, 1);
+	const onVerifyCode = (ev: Event) => {
+		const target = ev.target as HTMLIonInputElement;
+		const inputIndex = parseInt(target.getAttribute("data-index") || "0", 10);
+		const value = (ev.target as HTMLIonInputElement).value as string;
 
-    // Update the OTP array with the new value
-    const newOtp = [...otpInputs];
-    newOtp[inputIndex] = filteredValue;
-    setOtpInputs(newOtp);
+		// Ensure the value is a single digit
+		const filteredValue = value.replace(/\D/g, "").substring(0, 1);
 
-    // Move focus to the next input if available
-    const nextInputIndex = inputIndex + 1;
-    if (nextInputIndex < 6) {
-      const nextInput = ionInputEls.current[nextInputIndex];
-      if (nextInput) {
-        nextInput.setFocus();
-      }
-      console.log(newOtp[nextInputIndex], nextInputIndex, "--here");
-      if (newOtp[nextInputIndex] !== "" && nextInputIndex === 5) {
-        generatorRecaptcha();
-      }
-      console.log(nextInputIndex, "--next input");
-    }
-    // const inputCmp = ionInputEl.current;
-    // if (inputCmp !== null) {
-    //   generatorRecaptcha();
-    //   inputCmp.value = filteredValue;
-    // }
-  };
+		// Update the OTP array with the new value
+		const newOtp = [...otpInputs];
+		newOtp[inputIndex] = filteredValue;
+		setOtpInputs(newOtp);
 
-  const handleClearAll = () => {
-    setOtpInputs(["", "", "", "", "", ""]);
+		// Move focus to the next input if available
+		const nextInputIndex = inputIndex + 1;
+		if (nextInputIndex < 6 && newOtp[inputIndex] !== "") {
+			const nextInput = ionInputEls.current[nextInputIndex];
+			if (nextInput) {
+				nextInput.setFocus();
+			}
+		}
+	};
+	const handleClearAll = () => {
+		setOtpInputs(["", "", "", "", "", ""]);
 
-    const firstInputElement = ionInputEls.current[0];
-    if (firstInputElement) {
-      firstInputElement.setFocus();
-    }
-  };
+		const firstInputElement = ionInputEls.current[0];
+		if (firstInputElement) {
+			firstInputElement.setFocus();
+		}
+	};
 
-  const handleCloseModal = () => {
-    setIsModalOpen(false);
-  };
+	const handleCloseModal = () => {
+		setIsModalOpen(false);
+	};
 
-  return (
-    <IonPage id="get-code-page">
-      <IonContent>
-        <div className="container">
-          <div className="login-logo">
-            <img src={Logo} alt="Ionic logo" />
-          </div>
+	return (
+		<IonPage id="get-code-page">
+			<IonContent>
+				<div className="container">
+					<div className="login-logo">
+						<img src={Logo} alt="Ionic logo" />
+					</div>
 
-          <IonTitle className="txt-phone">
-            Enter your number we texted you
-          </IonTitle>
-          <form noValidate onSubmit={(event) => handleSubmit(event)}>
-            <div style={{ display: "flex", margin: "5px" }}>
-              {otpInputs.map((value, index) => (
-                <IonInput
-                  key={index}
-                  ref={(el) => (ionInputEls.current[index] = el)}
-                  placeholder="0"
-                  maxlength={1}
-                  inputMode="numeric"
-                  style={{
-                    color: "white",
-                    textAlign: "center",
-                    marginRight: "3px",
-                  }}
-                  value={value}
-                  onIonInput={onVerifyCode}
-                  data-index={index}
-                ></IonInput>
-              ))}
+					<IonTitle className="txt-phone">
+						Enter your number we texted you
+					</IonTitle>
+					<form noValidate onSubmit={(event) => handleSubmit(event)}>
+						<div style={{ display: "flex", margin: "5px" }}>
+							{otpInputs.map((value, index) => (
+								<IonInput
+									key={index}
+									ref={(el) => (ionInputEls.current[index] = el)}
+									placeholder="0"
+									maxlength={1}
+									inputMode="numeric"
+									style={{
+										color: "white",
+										textAlign: "center",
+										marginRight: "3px",
+									}}
+									value={value}
+									onIonInput={onVerifyCode}
+									data-index={index}
+								></IonInput>
+							))}
 
-              <IonButton onClick={handleClearAll} className="close-icon">
-                <IonIcon icon={closeOutline} />
-              </IonButton>
-            </div>
-            <div id="recaptcha-container"></div>
-            <div style={{ marginBottom: "50px" }} />
+							<IonButton onClick={handleClearAll} className="close-icon">
+								<IonIcon icon={closeOutline} />
+							</IonButton>
+						</div>
+						<div id="recaptcha-container"></div>
+						<div style={{ marginBottom: "50px" }} />
 
-            <IonButton type="submit" expand="block" className="button">
-              SUBMIT
-            </IonButton>
-            <div style={{ marginBottom: "200px" }} />
-          </form>
-        </div>
-        <IonModalComponent isOpen={isModalOpen} onClose={handleCloseModal} />
-      </IonContent>
-      <ToastContainer />
-    </IonPage>
-  );
+						<IonButton type="submit" expand="block" className="button">
+							SUBMIT
+						</IonButton>
+						<div style={{ marginBottom: "200px" }} />
+					</form>
+				</div>
+				<IonModalComponent isOpen={isModalOpen} onClose={handleCloseModal} />
+			</IonContent>
+			<ToastContainer />
+		</IonPage>
+	);
 };
 
 export default GetCode;
